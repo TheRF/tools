108f4d5a496fcf4e9bad712da913a5a5437f1bf4
Chris <r_fast@web.de>
2017-10-06 13:00:05 +0200

    Print usage page when parameter wrong

--- hstartup.hs
+++ hstartup.hs
@@ -3,14 +3,26 @@ import System.Process
 import System.Environment
 import Control.Monad
 import Control.Exception
+import System.Directory
 
 -- start new processes
 -- reads programs out of a text file and starts them
 -- can also split parameters for those programs
 main::IO ()
 main = do
-  [s] <- getArgs
-  f   <- readFile s
+  s <- getArgs
+  bf <- doesFileExist $ head' s
+  if (length s == 1) && bf
+    then readConfig $ head' s
+    else printHelp
+
+head'::[String]->String
+head' [] = ""
+head' (x:xs) = x
+
+readConfig::String->IO ()
+readConfig s = do
+  f <- readFile s
   let paths = lines f 
       x = map head (map (splitOnChar (==',')) paths)
       xs= map (drop 1) (map (splitOnChar (==',')) paths)
@@ -37,3 +49,8 @@ startProc pr params= do
   case result of
     Left ex -> putStrLn $ "error starting: "++ show ex
     Right (_,_,_,p) -> putStrLn "started ok"
+
+printHelp::IO ()
+printHelp = do
+  putStrLn "------- hstartup tool -------"
+  putStrLn "usage: hstartup <config file>"
